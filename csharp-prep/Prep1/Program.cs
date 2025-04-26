6a0c85079a4ec6990bf74bd97b323fa2bf003e2e
PCall1 <pcall1@byui.edu>
2025-04-26 09:17:07 -0600

    timing

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,21 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!"); // This line is not needed for the task
 
         // Prompt the user for their first name
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
         // Add a delay in milliseconds
-        Thread.Sleep(500);
+        Thread.Sleep(700);
 
         //Prompt the user for their last name
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
             Console.WriteLine(); //enter a line for formatting
-            Thread.Sleep(1500);
+            Thread.Sleep(1500); //1.5 second delay 
 
         // Display the user's name in the 007 format "lastName, firstName lastName"
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
