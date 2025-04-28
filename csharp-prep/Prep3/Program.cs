873295860c08116e91a15374f3dfb08015cee3f6
PCall1 <pcall1@byui.edu>
2025-04-28 10:42:49 -0600

    number guess game v1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.CodeDom.Compiler;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //get number from user
+        Console.Write("Enter a number between 1 and 100: ");
+        string intput = Console.ReadLine();
+        int number = int.Parse(intput);
+
+        // generate random number between 1 and 100
+        Random random = new Random();
+        int rndMagicNumber = random.Next(1, 101);
+
+
+
+        int guess = -1; //
+
+        // loop until guess is correct
+        while (guess != number)
+        {
+
+            // ask user for guess
+            Console.Write("Guess a number between 1 and 100: ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+                // check if guess is correct
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher! Guess again: ");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower! Guess again: ");
+                }
+                else if (guess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+
+            
+
+            
+
+
+
+        }
+        
     }
 }
