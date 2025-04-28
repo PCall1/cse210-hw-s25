7fa4f8d043b3a4755fbcbd4bdfe359911a750b6d
PCall1 <pcall1@byui.edu>
2025-04-28 10:46:35 -0600

    changed outputs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -30,15 +30,15 @@ class Program
                 // check if guess is correct
                 if (guess < number)
                 {
-                    Console.WriteLine("Higher! Guess again: ");
+                    Console.WriteLine("Higher!");
                 }
                 else if (guess > number)
                 {
-                    Console.WriteLine("Lower! Guess again: ");
+                    Console.WriteLine("Lower!");
                 }
                 else if (guess == number)
                 {
-                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine("You got it!");
                 }
 
 
